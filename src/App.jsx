51263a2bc81229d15c280a4289e1476e7c41f1e6
roji-tech <rojitech9@gmail.com>
2022-07-03 15:24:03 +0100

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,14 @@
-import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import Home from "./components/home/Home";
-import Navbar from "./components/navbar/Navbar";
-import Sidedrawer from "./components/navbarmobile/SideDrawer";
-import Poplink from "./components/poplink/poplink";
-import Backdrop from "./components/backdrop/backdrop";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
-
-class App extends Component {
-  state = {
-    sideDrawerOpen: false
-  };
-
-  sideDrawerHandler = () => {
-    this.setState((prevState) => {
-      return { sideDrawerOpen: !prevState.sideDrawerOpen };
-    });
-  };
-
-  render() {
-    let sideDrawer;
-
-    if (this.state.sideDrawerOpen) {
-      sideDrawer = <Sidedrawer />
-    }
-
-    return (
-      <Router>
-        <Navbar drawerClickHandler={this.sideDrawerHandler} />
-        {sideDrawer}
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-        </Routes>
-        {/* <Poplink /> */}
-        {/* <Backdrop /> */}
-      </Router>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
