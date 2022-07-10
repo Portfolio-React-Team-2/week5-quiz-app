18c7a41a0cee9ce4bfc1c5dd0bfcc411babb089c
roji-tech <rojitech9@gmail.com>
2022-07-10 05:12:39 +0100

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
-import DataFunc from "./components/DataContext";
 
 const App = () => {
-  const { gameState, setGameState } = DataFunc();
 
   return (
     <Router>
